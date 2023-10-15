67b96ad080e7fb8db65ba429be6001bc5f73d258
Vera-VZ-R <128039427+Vera-VZ-R@users.noreply.github.com>
2023-10-15 11:09:44 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>}>
+        <Route path="/marigolds" element={<Layout/>}>
           <Route index element={<Home/>} />
           <Route path="services" element={<Services/>} />
           <Route path="price" element={<Price/>} />
